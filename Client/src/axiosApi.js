5a967e5adc37e2b872616903c1311ecdf3004a66
Chintan <chintansanghvi5@gmail.com>
2019-09-15 19:57:25 -0700

    Changing axios base URL to / so that it can work with server

--- Client/src/axiosApi.js
+++ Client/src/axiosApi.js
@@ -4,7 +4,7 @@ const axiosApi = axios.create({
   baseURL:
     process.env.REACT_APP_BASE_URL !== undefined
       ? process.env.REACT_APP_BASE_URL
-      : 'http://localhost:4242/'
+      : '/'
 })
 
 export default axiosApi
